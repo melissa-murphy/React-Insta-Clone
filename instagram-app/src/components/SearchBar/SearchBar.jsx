a56880abf61e921fa8a63c7e1dec5310767f82ec
melissa-murphy <melissamurphy.ca@gmail.com>
2019-04-29 12:36:09 -0700

    component files + SearchBar loading to app

---
+++ instagram-app/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const propTypes = {};
+
+const SearchBar = () => {
+  return (
+      <>
+      <h1>Hello World</h1>
+      </>
+  );
+};
+
+SearchBar.propTypes = propTypes;
+
+export default SearchBar;
